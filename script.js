8f11df0b0f3c21533c37aff9edae74410a0d627b
Toyibu Olaniyi, ABIDOGUN <toyibuabidogun@gmail.com>
2022-04-10 07:31:34 +0100

    Include backspace key

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ function calcResult(){
 function clearDisplay(){
     display.value = "";
 }
+
+function backspaceKey(){
+    display.value = display.value.slice(0,-1);
+}
+
+function nan(){
+    if(display.value == "Infinity"){
+        display.value = "naN - You cannot divide by zero."
+    }
+}
